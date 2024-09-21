f75ce48499139136e784e604563e58426813d0ad
Tom Gobich <tom@tomgobich.com>
2024-09-21 11:37:15 -0400

    4.6 - Completing Our AppLayout and Navigation

---
+++ inertia/components/Navigation.vue
@@ -0,0 +1,119 @@
+<script lang="ts" setup>
+import { Menu, Slash, Route } from 'lucide-vue-next'
+</script>
+
+<template>
+  <nav class="hidden gap-5 text-sm items-center md:flex lg:gap-6">
+    <Link href="/courses" class="flex items-center gap-2 text-lg font-semibold md:text-base">
+      <Route class="h-6 w-6" />
+      <span class="sr-only">PlotMyCourse</span>
+    </Link>
+
+    <div class="flex items-center">
+      <Slash class="text-slate-300 w-4 h-4 -rotate-12" />
+      <span>TODO: ORG SELECT</span>
+      <Slash class="text-slate-300 w-4 h-4 -rotate-12" />
+    </div>
+
+    <Link
+      href="/courses"
+      class="desktop-link"
+      :class="{ active: $page.url.startsWith('/courses') }"
+    >
+      Courses
+    </Link>
+
+    <Link
+      href="/difficulties"
+      class="desktop-link"
+      :class="{ active: $page.url.startsWith('/difficulties') }"
+    >
+      Difficulties
+    </Link>
+
+    <Link
+      href="/statuses"
+      class="desktop-link"
+      :class="{ active: $page.url.startsWith('/statuses') }"
+    >
+      Statuses
+    </Link>
+
+    <Link
+      href="/access-levels"
+      class="desktop-link"
+      :class="{ active: $page.url.startsWith('/access-levels') }"
+    >
+      Access Levels
+    </Link>
+  </nav>
+
+  <Sheet>
+    <SheetTrigger as-child>
+      <Button variant="outline" size="icon" class="shrink-0 md:hidden">
+        <Menu class="w-5 h-5" />
+        <span class="sr-only">Toggle navigation menu</span>
+      </Button>
+    </SheetTrigger>
+    <SheetContent side="left">
+      <nav class="grid gap-6 text-lg font-medium">
+        <Link href="/courses" class="flex items-center gap-2 text-lg font-semibold">
+          <Route class="h-6 w-6" />
+          <span class="sr-only">PlotMyCourse</span>
+        </Link>
+
+        <span>TODO: ORG SELECT</span>
+
+        <Link
+          href="/courses"
+          class="mobile-link"
+          :class="{ active: $page.url.startsWith('/courses') }"
+        >
+          Courses
+        </Link>
+
+        <Link
+          href="/difficulties"
+          class="mobile-link"
+          :class="{ active: $page.url.startsWith('/difficulties') }"
+        >
+          Difficulties
+        </Link>
+
+        <Link
+          href="/statuses"
+          class="mobile-link"
+          :class="{ active: $page.url.startsWith('/statuses') }"
+        >
+          Statuses
+        </Link>
+
+        <Link
+          href="/access-levels"
+          class="mobile-link"
+          :class="{ active: $page.url.startsWith('/access-levels') }"
+        >
+          Access Levels
+        </Link>
+      </nav>
+    </SheetContent>
+  </Sheet>
+</template>
+
+<style scope>
+.desktop-link {
+  @apply text-muted-foreground transition-colors hover:text-foreground;
+
+  &.active {
+    @apply text-foreground;
+  }
+}
+
+.mobile-link {
+  @apply text-muted-foreground hover:text-foreground;
+
+  &.active {
+    @apply text-foreground;
+  }
+}
+</style>
--- inertia/layouts/AppLayout.vue
+++ inertia/layouts/AppLayout.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import UserDto from '#dtos/user'
+import { Link } from '@inertiajs/vue3'
+import { UserCircle } from 'lucide-vue-next'
 
 const props = defineProps<{
   user: UserDto
@@ -7,10 +9,44 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div>
-    <h1>AppLayout</h1>
-    <div>
+  <div class="flex min-h-screen w-full flex-col">
+    <header
+      class="sticky top-0 z-50 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6"
+    >
+      <Navigation v-bind="props" />
+
+      <!-- User Panel -->
+      <div class="flex flex-1 items-center justify-end">
+        <DropdownMenu>
+          <DropdownMenuTrigger as-child>
+            <Button variant="secondary" size="icon" class="rounded-full">
+              <UserCircle class="w-5 h-5" />
+              <span class="sr-only">Toggle user menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>USER NAME</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem :as="Link" href="/settings/profile">
+              Profile Settings
+            </DropdownMenuItem>
+            <DropdownMenuItem :as="Link" href="/settings/account">
+              Account Settings
+            </DropdownMenuItem>
+            <DropdownMenuItem :as="Link" href="/settings/organization">
+              Organization Settings
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem :as="Link" href="/logout" method="post"> Logout </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </header>
+
+    <main
+      class="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10"
+    >
       <slot />
-    </div>
+    </main>
   </div>
 </template>
