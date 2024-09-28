3690235b53adbcf96dedb6d6c6c887a46e542f66
Tom Gobich <tom@tomgobich.com>
2024-09-28 09:28:19 -0400

    2.9-01 - updating server-sent title info to match the client

--- inertia/app/ssr.ts
+++ inertia/app/ssr.ts
@@ -4,9 +4,12 @@ import { createSSRApp, h, type DefineComponent } from 'vue'
 import { Link } from '@inertiajs/vue3'
 import AppLayout from '~/layouts/AppLayout.vue'
 
+const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
+
 export default function render(page: any) {
   return createInertiaApp({
     page,
+    title: (title) => `${title} - ${appName}`,
     render: renderToString,
     resolve: (name) => {
       const pages = import.meta.glob<DefineComponent>('../pages/**/*.vue', { eager: true })
